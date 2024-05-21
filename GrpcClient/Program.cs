566ebf29787359553e75a412dc7fc05faffc20d3
André Pereira Fonseca <afonseca@fatorseguradora.com.br>
2024-05-21 09:48:13 -0300

    Initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+var channel = GrpcChannel.ForAddress("https://localhost");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest { Name = "André Fonseca" });
+
+Console.WriteLine(response.Message);
+
+Console.ReadLine();
